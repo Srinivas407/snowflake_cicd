b8d8e29742f5cacb3380ca6cdae8785c8f7b1630
Srinivas407 <67363904+Srinivas407@users.noreply.github.com>
2025-02-26 18:27:08 +0530

    Update srini_first_cicd_model.sql

--- dbscripts/srini_first_cicd_model.sql
+++ dbscripts/srini_first_cicd_model.sql
@@ -5,6 +5,6 @@
 
 with srini_schema_change_test as(
 
-	select * from SRINI_P1.SRINI_P2.NETFLIX_DATA where type = 'Movie'
+	select * from SRINI_P1.SRINI_P2.NETFLIX_DATA where type = 'TV Show'
 )
 select * from srini_schema_change_test
