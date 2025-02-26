02c1d6adb714a485b0aace30af8865eb6a8974fb
Srinivas407 <67363904+Srinivas407@users.noreply.github.com>
2025-02-26 17:44:04 +0530

    Create first_model.sql

---
+++ first_model.sql
@@ -0,0 +1,10 @@
+{{	config(	materialized = 'table',
+		alias = 'srini_schema_change_test'
+	)
+}}
+
+with srini_schema_change_test as(
+	select * from SRINI_P1.SRINI_P2.NETFLIX_DATA where type = 'Movie'
+)
+
+select * from srini_schema_change_test
